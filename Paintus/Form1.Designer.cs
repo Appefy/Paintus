93fed95e3abb0ace6a13dee5807ff2a57e71c50f
PRITHWIRAJ SAMANTA <prskid1000@gmail.com>
2020-09-28 08:08:45 +0530

    File Structure Updated

---
+++ Paintus/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Paintus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(263, 87);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "File";
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.ForestGreen;
+            this.button8.Location = new System.Drawing.Point(178, 26);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(73, 44);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "SAVE";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.ForestGreen;
+            this.button7.Location = new System.Drawing.Point(89, 26);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(83, 44);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "SAVE AS";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.ForestGreen;
+            this.button6.Location = new System.Drawing.Point(6, 26);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 44);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "OPEN";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.ForestGreen;
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.button13);
+            this.groupBox3.Controls.Add(this.button12);
+            this.groupBox3.Controls.Add(this.button10);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox3.Location = new System.Drawing.Point(281, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(220, 87);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Draw";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox2.Location = new System.Drawing.Point(147, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(62, 34);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(79, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 34);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.ForestGreen;
+            this.button13.Location = new System.Drawing.Point(147, 41);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(63, 28);
+            this.button13.TabIndex = 3;
+            this.button13.Text = "ERASE";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.ForestGreen;
+            this.button12.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button12.Location = new System.Drawing.Point(79, 41);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(62, 28);
+            this.button12.TabIndex = 2;
+            this.button12.Text = "PEN";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.ForestGreen;
+            this.button10.Location = new System.Drawing.Point(6, 25);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(67, 44);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "COLOR";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(12, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(996, 461);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(1017, 578);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.857143F);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Painto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Paintus/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Paintus
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics g;
+        Graphics g1;
+        string cur = "";
+        Point sp, ep;
+        Pen p=new Pen(Color.White,1);
+        int size = 1;
+        Color cc=Color.White;
+        bool ismd = false;
+        Stack stack = new Stack();
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "1";
+            textBox2.Text = "10";
+            bmp = new Bitmap(996, 461);
+            g = Graphics.FromImage(bmp);
+            g1 = panel1.CreateGraphics();
+            p = new Pen(cc, size);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //cut
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //save
+            bmp.Save(cur, System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //erase
+            p = new Pen(Color.Black,int.Parse(textBox2.Text));
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //undo
+  
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            size = int.Parse(textBox1.Text);
+            p = new Pen(cc, size);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //redo
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //copy
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //paste
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //open
+            openFileDialog1.Title = "Open";
+            openFileDialog1.AddExtension = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.DefaultExt = ".bmp";
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //new
+
+            saveFileDialog1.Title = "New";
+            saveFileDialog1.AddExtension = true;
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt= ".bmp";
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //color
+            colorDialog1.ShowDialog();
+            cc = colorDialog1.Color;
+            p = new Pen(colorDialog1.Color,size);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            cur = saveFileDialog1.FileName;
+            bmp.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            cur = openFileDialog1.FileName;
+            bmp = new Bitmap(cur);
+            Bitmap temp = new Bitmap(bmp);
+            bmp.Dispose();
+            bmp = new Bitmap(temp);
+            g = Graphics.FromImage(bmp);
+            g1.DrawImage(bmp, 0, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ismd = true;
+            sp = e.Location;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(ismd==true)
+            {
+                ep = e.Location;
+                g1.DrawLine(p,sp,ep);
+                g.DrawLine(p, sp, ep);
+                sp = ep;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            if (ismd == true)
+            {
+                ep = e.Location;
+                ismd = false;
+            } 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
